77011a3cef021a7cee51fae64ced9d0104da2604
MatthewRMackey <55336869+MatthewRMackey@users.noreply.github.com>
2019-11-12 13:48:07 -0600

    Add files via upload
    This is the teaching website files

---
+++ Teaching/teaching.js
@@ -0,0 +1,145 @@
+let featureExtractor;
+let classifier;
+let video;
+let loss;
+let images=0;
+
+function setup() {
+  noCanvas();
+  // Create a video element
+  video = createCapture(VIDEO);
+  // Append it to the videoContainer DOM element
+  video.parent('videoContainer');
+  // Extract the already learned features from MobileNet
+  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+  // Create a new classifier using those features and give the video we want to use
+  const options = { numLabels: 26 };
+  classifier = featureExtractor.classification(video, options);
+  // Set up the UI buttons
+  setupButtons();
+}
+
+// A function to be called when the model has been loaded
+function modelReady() {
+  select('#modelStatus').html('Base Model (MobileNet) Loaded!');
+  classifier.load('./model/model.json', function() {
+    select('#modelStatus').html('Custom Model Loaded!');
+  });
+}
+
+// A function to be called when the video has loaded
+function videoReady() {
+  select('#videoStatus').html('Video ready!');
+}
+
+// Classify the current frame.
+function classify() {
+  classifier.classify(gotResults);
+}
+
+//Draw a frame around hands detected in the screen
+function runHandDetection(){
+  handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+            updateNote.innerText = "Video started. Now tracking"
+            isVideo = true
+            runDetection()
+        } else {
+            updateNote.innerText = "Please enable video"
+        }
+    });
+  
+  model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+    });
+}
+
+// A util function to create UI buttons
+function setupButtons() {
+  buttontry=select('#trybtn')
+  buttontry.mousePressed(function(){
+     var str1 = "ABabc";
+    var array = str1.split("");
+    var n = Math.round(Math.random() * (array.length - 1));  
+
+    document.getElementById("letter").innerHTML=array[n];
+  });
+  buttontest=select('#testbtn')
+  buttontest.mousePressed(function(){
+    var lt = document.getElementById("result").innerHTML;
+    var l = document.getElementById("letter").innerHTML;
+    if(lt==l){document.getElementById("R").innerHTML="Good";
+           }else{document.getElementById("R").innerHTML="Wrong"}
+  });
+  buttonsubmit=select('#submit')
+  buttonsubmit.mousePressed(function(){
+    var c = document.getElementById("value").value;
+    document.getElementById("input").innerHTML=c;
+    images=0;
+  });
+
+  buttonadd=select('#addimage')
+  buttonadd.mousePressed(function(){
+    var c = document.getElementById("value").value;
+    
+    var input = c.toString();
+    classifier.addImage(input);
+    select('#amount').html(images++);
+
+  });
+
+  train = select('#train');
+  train.mousePressed(function() {
+    classifier.train(function(lossValue) {
+      if (lossValue) {
+        loss = lossValue;
+        select('#loss').html('Loss: ' + loss);
+      } else {
+        select('#loss').html('Done Training! Final Loss: ' + loss);
+      }
+    });
+  });
+
+  // Predict Button
+  buttonPredict = select('#buttonPredict');
+  buttonPredict.mousePressed(classify);
+
+  // Save model
+  saveBtn = select('#save');
+  saveBtn.mousePressed(function() {
+    classifier.save();
+  });
+
+  // Load model
+  loadBtn = select('#load');
+  loadBtn.changed(function() {
+    classifier.load(loadBtn.elt.files, function(){
+      select('#modelStatus').html('Custom Model Loaded!');
+    });
+  });
+}
+
+// Show the results
+function gotResults(err, results) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+  if (results && results[0]) {
+    select('#result').html(results[0].label);
+    select('#confidence').html(results[0].confidence);
+    classify();
+  }
+}
+
+handTrack.load(modelParams).then(lmodel => {
+    // detect objects in the image.
+    model = lmodel
+    updateNote.innerText = "Loaded Model!"
+    trackButton.disabled = false
+});
